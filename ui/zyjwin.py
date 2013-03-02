dfd0cfdb0de05083a142b164ed76e22dd3ec1ec4
kerneltravel <kjpioo2006@gmail.com>
2013-03-02 10:24:23 +0800

    实现了：根据关键词搜索结果 实时更新界面的book列表和parts列表

--- ui/zyjwin.py
+++ ui/zyjwin.py
@@ -27,6 +27,7 @@ class ZyjWin(QMainWindow, Ui_ZyjWin):
         self.setupUi(self)
         #self.keywd = ""
         #self.data = ZyjDataOp()
+        self.modelAll = QtSql.QSqlQueryModel()
         
         self.model = QtSql.QSqlQueryModel()
         self.model.setQuery('select docid,title,content from articles_vt limit 0,%s'%self.nPagePer)
@@ -50,13 +51,88 @@ class ZyjWin(QMainWindow, Ui_ZyjWin):
         else:
             #self.model.doQuery(self.keywd)
             #qstringKeywd = QString(self.keywd)
-            queryStr = "select docid,title,content from articles_vt where articles_vt match '%s' limit %s,%s"% (self.keywd, 
-                                                                                                                (self.nPageCur-1)*self.nPagePer, self.nPagePer)
+            queryStrAll = "select docid,title,content from articles_vt where articles_vt match '%s' "%(self.keywd)
+            queryStr =  queryStrAll + "limit %s,%s"% ((self.nPageCur-1)*self.nPagePer, self.nPagePer)
             #sql = "SELECT * FROM server WHERE platform='%s'" % platform
             print queryStr
             self.model.setQuery(queryStr)
             self.contentTableview.setModel(self.model)
             print self.model.rowCount()
+            
+            '''开始根据计算 结果总行数。可以显示到界面上。翻页用到
+            '''
+            self.modelAll.setQuery("select count(docid) from articles_vt where articles_vt match '%s'"%(self.keywd))
+            rowcount = self.modelAll.record(0).value(0).toInt()[0]
+            self.nPageCount = rowcount/self.nPagePer
+            if rowcount > 0 and  self.nPageCount > 0:
+                if rowcount%self.nPageCount > 0:
+                    self.nPageCount = self.nPageCount +1
+            else:
+                self.nPageCount = 1   
+            print '%s,%s'%(rowcount, self.nPageCount)
+            '''开始根据docid生成 bookCombox内的book列表和 bookpartCombox内的bookparts列表
+            '''
+            self.modelAll.clear()
+            self.modelAll.setQuery("select docid from articles_vt where articles_vt match '%s'"%self.keywd)
+            i = 0
+            '''bookdoclst存储 book列表'''
+            bookdoclst = [] 
+            while i< rowcount:
+                #print 'i:%s'%i
+                if i%self.nPagePer==0:
+                    self.modelAll.fetchMore()
+                bookdoclst.append(self.modelAll.record(i).value(0).toInt()[0])
+                i = i+1
+            i = 0
+            
+            '''计算搜索结果对应的bookpart列表'''
+            querystr = ""
+            while i< rowcount:
+                querystr = querystr +('%s OR '%bookdoclst[i])
+                i = i+1
+            querystr = querystr.rstrip('OR ')
+            querystr = "select docid,name from bkparts_vt where articleids match '%s'"%( querystr)
+            self.modelAll.clear()
+            self.modelAll.setQuery(querystr)
+            print 'querystr: %s'%querystr
+            while self.modelAll.canFetchMore():
+                #print 'i:%s'%i
+                self.modelAll.fetchMore()
+
+            i = 0
+            #partIDlst = []
+            self.bookpartCombox.clear()
+            while i<self.modelAll.rowCount():
+                #print "%s"%self.modelAll.record(i).value(1).toString()
+                #self.bookpartCombox.addItem(self.modelAll.record(i).value(0).toString()[0])
+                self.bookpartCombox.addItem("%s"%(self.modelAll.record(i).value(1).toString()),"%d"%(self.modelAll.record(i).value(0).toInt()[0]))
+                #print 'doclist[%s]= %s'%(i, doclst[i])
+                i = i+1
+                
+            '''计算book列表'''
+            querystr=""
+            i = 0
+            while i < self.modelAll.rowCount():
+                querystr = querystr+('%s OR '%(self.modelAll.record(i).value(0).toInt()[0]))
+                i = i+1
+            querystr = querystr.rstrip('OR ')
+            querystr = "select docid,name from books_vt where partids match '%s'"%( querystr)
+            self.modelAll.clear()
+            self.modelAll.setQuery(querystr)
+            print 'querystr: %s'%querystr
+            while self.modelAll.canFetchMore():
+                #print 'i:%s'%i
+                self.modelAll.fetchMore()
+
+            i = 0
+            #partIDlst = []
+            self.bookCombox.clear()
+            while i<self.modelAll.rowCount():
+                #print "%s"%self.modelAll.record(i).value(1).toString()
+                #self.bookCombox.addItem(self.modelAll.record(i).value(0).toString()[0])
+                self.bookCombox.addItem("%s"%(self.modelAll.record(i).value(1).toString()),"%d"%(self.modelAll.record(i).value(0).toInt()[0]))
+                #print 'doclist[%s]= %s'%(i, doclst[i])
+                i = i+1
             
     @QtCore.pyqtSignature("")
     def on_bookCombox_Changed(self):
