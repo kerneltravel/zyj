d30f2bb53d7a3e49e7f19a0c12fe7ac9a166679a
kerneltravel <kjpioo2006@gmail.com>
2013-03-12 18:05:21 +0800

    增加对 过滤 数据库中获得的显示文字的过滤，去掉多余的符号，规范化换行

--- ui/viewArticle.py
+++ ui/viewArticle.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from Ui_viewarticle import Ui_viewArticle
 from PyQt4 import QtCore, QtGui, QtSql
 from PyQt4.QtGui import *
-
+from PyQt4.QtCore import *
 class ViewArticle(QDialog, Ui_viewArticle):
     def __init__(self, title="", content="", parent = None):
         #QMainWindow.__init__(self, parent)
@@ -11,7 +13,12 @@ class ViewArticle(QDialog, Ui_viewArticle):
         #viewArticle = QtGui.QDialog()
         self.setupUi(self)
         self.setWindowTitle(title)
-
+        
+        if content is not "":
+            print content.toLocal8Bit()
+            content = content.replace(u" \r\n", u"")
+            content = content.replace(u"\\x", u"\n\n")
+            #print content.toLocal8Bit()
         self.articleContent.setPlainText(content)
         #self.articleContent.
         #self.show()
