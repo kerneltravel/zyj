9c744babac4a5bec7b9636685e21f456f3e79784
kerneltravel <kjpioo2006@gmail.com>
2013-05-17 18:26:40 +0800

    + 文章窗口增加了HTML方式显示

--- ui/viewArticle.py
+++ ui/viewArticle.py
@@ -5,7 +5,18 @@ from PyQt4 import QtCore, QtGui, QtSql
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 class ViewArticle(QDialog, Ui_viewArticle):
-    def __init__(self, title="", content="", parent = None):
+    def getKeywordsHtml(self, kws, content, fontsize=13):
+        if len(content)!=0:
+            kws.replace(u" ", u"")
+            for word in kws:
+                newwd = u"<font size=%d><strong>%s</strong></font>"% (fontsize,word)
+                content = content.replace(word, newwd)
+            
+            print content.toLocal8Bit()
+        return content
+            
+
+    def __init__(self, keywords="", title="", content="", parent = None):
         #QMainWindow.__init__(self, parent)
         #Ui_viewArticle.__init__(self)
         super(ViewArticle, self).__init__(parent)
@@ -14,11 +25,14 @@ class ViewArticle(QDialog, Ui_viewArticle):
         self.setupUi(self)
         self.setWindowTitle(title)
         
-        if content is not "":
+        #self.keywd = u"感冒"
+        self.content = ""
+        
+        if len(content) != 0:
             print content.toLocal8Bit()
             content = content.replace(u" \r\n", u"")
             content = content.replace(u"\\x", u"\n\n")
             #print content.toLocal8Bit()
-        self.articleContent.setPlainText(content)
-        #self.articleContent.
-        #self.show()
+        #self.articleContent.setPlainText(content)
+        self.articleContent.appendHtml(self.getKeywordsHtml(keywords, content))
+        self.articleContent.moveCursor(QTextCursor.Start)
